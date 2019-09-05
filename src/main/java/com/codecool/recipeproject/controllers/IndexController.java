2da83b144aee31ae8c1d5d54b8df256ea6b5df3b
farkaspeti <farkas.peti97@gmail.com>
2019-09-05 14:08:16 +0200

    Controllers and Services use @Sl4j from lombok now

--- src/main/java/com/codecool/recipeproject/controllers/IndexController.java
+++ src/main/java/com/codecool/recipeproject/controllers/IndexController.java
@@ -5,12 +5,14 @@ import com.codecool.recipeproject.domain.UnitOfMeasure;
 import com.codecool.recipeproject.repositories.CategoryRepository;
 import com.codecool.recipeproject.repositories.UnitOfMeasureRepository;
 import com.codecool.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
     
@@ -22,6 +24,7 @@ public class IndexController {
     
     @RequestMapping({"","/","/index"})
     public String getIndexPage(Model model){
+        log.debug("i reached the index page");
        model.addAttribute("recipes",recipeService.getRecepies());
     
         
